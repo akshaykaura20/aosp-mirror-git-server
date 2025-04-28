076e45d36b4574e4979abac3a79f7774d60ddf78
akshay-kaura <akshay.kaura@accenture.com>
2025-04-28 23:12:54 +0530

    - Shifted health check (liveness) script to a separate file; - increased health_check interval, timeout and unhealthy thresholds (max retries)

---
+++ scripts/liveness-check.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Waiting for VM to boot and complete startup scripts..."
+MAX_SERVER_DISCOVERY_RETRY=20  # Wait up to 10 minutes (20 * 30s)
+SERVER_DISCOVERY_RETRY=0
+SLEEP_TIME=30
+while true; do
+if curl --fail -v https://${MIRROR_LB_IP}/healthcheck; then
+    echo "Server is healthy!"
+    exit 0
+fi
+SERVER_DISCOVERY_RETRY=$((SERVER_DISCOVERY_RETRY + 1))
+if [ $CONTAINER_DISCOVERY_RETRY -ge $MAX_SERVER_DISCOVERY_RETRY ]; then
+    echo "[ERROR] Timed out waiting for Server to become healthy."
+    exit 1
+fi
+echo "Attempt $SERVER_DISCOVERY_RETRY: Server Not healthy yet, retrying in ${SLEEP_TIME}s..."
+sleep $SLEEP_TIME
+done
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -13,10 +13,10 @@ locals {
   backend_service_timeout_sec = 30
 
   health_check = {
-    path               = "/healthcheck"
-    check_interval_sec = 10
-    timeout_sec        = 5
-    healthy_threshold    = 3
-    unhealthy_threshold  = 3
+    path                 = "/healthcheck"
+    check_interval_sec   = 30
+    timeout_sec          = 10
+    healthy_threshold    = 2
+    unhealthy_threshold  = 14 # scripts take time to finish
   }
 }
